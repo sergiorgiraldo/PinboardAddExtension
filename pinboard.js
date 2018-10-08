3c562d82de6ed0e634ed953eb5db6b64dd11d1d9
Sérgio Giraldo <sergiorgiraldo@gmail.com>
2018-10-08 16:37:12 -0300

    refactoring

--- pinboard.js
+++ pinboard.js
@@ -1,19 +1,19 @@
 window.onload=function(){
-	if (location.href.indexOf("www.google.com/search") == -1 && 
-		location.href.indexOf("www.google.com.br/search") == -1 &&
-		location.href.indexOf("pinboard.in") == -1 &&
-		location.href.indexOf("whatsapp") == -1 &&
-		location.href.indexOf("mail.google") == -1 &&
-		location.href.indexOf("outlook.live") == -1 &&
-		location.href.indexOf(".intranet") == -1 
-		){
+	sitesToExclude = ['www.google.com/search', 
+			'www.google.com.br/search', 
+			'pinboard.in', 
+			'whatsapp', 
+			'mail.google', 
+			'outlook.live', 
+			'intranet'];
+
+	if (!sitesToExclude.includes(location.href)){
 		_url = encodeURIComponent(location.href)
 		_description = encodeURIComponent(document.title)
 		_dt = new Date().getFullYear().toString() + ('0' + (new Date().getMonth() + 1)).slice(-2) + ('0' + new Date().getDate()).slice(-2) + ' ' + new Date().getFullYear().toString() + ('0' + (new Date().getMonth() + 1)).slice(-2)
 
 		var xhr = new XMLHttpRequest();
-		xhr.open("GET", "https://api.pinboard.in/v1/posts/add?auth_token=PEGAR_TOKEN_NOS_SETTINGS_DO_SITE&url=" + _url + "&description=" + _description + "&tags=navigation " + _dt, true);
-
+		xhr.open("GET", "https://api.pinboard.in/v1/posts/add?auth_token=PEGAR_NO_SITE&url=" + _url + "&description=" + _description + "&tags=navigation " + _dt, true);
 		xhr.send();
 	}
 }
