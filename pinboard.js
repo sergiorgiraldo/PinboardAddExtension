6a5f0436e48aab82fe62d2c4e2d3d591f9f55e31
Sérgio Giraldo <sergiorgiraldo@gmail.com>
2018-10-08 16:24:44 -0300

    more sites to exclude

--- pinboard.js
+++ pinboard.js
@@ -1,5 +1,9 @@
 window.onload=function(){
-	if (location.href.indexOf("www.google.com/search") == -1 && location.href.indexOf("www.google.com.br/search") == -1){
+	if (location.href.indexOf("www.google.com/search") == -1 && 
+		location.href.indexOf("www.google.com.br/search") == -1 &&
+		location.href.indexOf("pinboard.in") == -1 &&
+		location.href.indexOf(".intranet") == -1 
+		){
 		_url = encodeURIComponent(location.href)
 		_description = encodeURIComponent(document.title)
 		_dt = new Date().getFullYear().toString() + ('0' + (new Date().getMonth() + 1)).slice(-2) + ('0' + new Date().getDate()).slice(-2) + ' ' + new Date().getFullYear().toString() + ('0' + (new Date().getMonth() + 1)).slice(-2)
